dc0c0a82fa1a6e47485b4ea9636bd4d6f2a5621b
Ryan <ryanjcua@msn.com>
2019-08-25 22:54:38 -0700

    Finish course info

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = (props) =>{
+    console.log(props);
+    return (<h1>{props.course}</h1>);
+};
+const Content = (props) =>{
+    const parts = props.parts;
+    const list_of_parts = parts.map(value => <Part name={value.name} exercises={value.exercises}/>)
+    return (
+    <div>
+        {list_of_parts}
+    </div>);
+};
+const Part = (props) => {
+    return (<p>{props.name} {props.exercises}</p>)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+};
+const Total = (props) =>{
+
+    return (<p>Number of exercises {props.total}</p>)
+};
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const parts = [{
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }];
+    let total_exercises = 0;
+    parts.forEach(value => {
+        total_exercises += value.exercises;
+    });
+
+    return (
+    <div>
+        <Header course={course}/>
+        <Content parts={parts}/>
+        <Total total={total_exercises}/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- part1/part1_tutorial/src/App.js
+++ part1/part1_tutorial/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const Hello = (props) => {
+  return (<div>
+          <p>Hello there, {props.name}</p>
+          </div>  )
+        };
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Greeting</h1>
+      <Hello name="Obi wan"/>
+      <Hello name="John"/>
+
     </div>
   );
 }
